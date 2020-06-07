c8e5423142f7e1500ac3fd862fc38fdc3313fc7f
mysterious-ranger301 <66529219+mysterious-ranger301@users.noreply.github.com>
2020-06-07 11:53:32 -0600

    Update SPL_for_linux.py

--- SPL_for_linux.py
+++ SPL_for_linux.py
@@ -12,7 +12,8 @@ Created on Wed Jun  3 18:35:00 2020
 @author: Mysterious Ranger
 SPL - subprocess library simplified
 Linux Version
-Command line functions are disabled (commented) so uncomment them and try to replicate them
+Some command line features are renamed as of Linux
+This version uses 2 modules for reliability
 """
 import subprocess as sp, os
 
@@ -22,29 +23,32 @@ class cmd:
 #        self.__author__ = spl.__author__
         self.__instructions__ = '''Unlike SPL, the cmd() class uses the command line to do things. SPL's Open() function isn't reliable for opening files with extensions like .mp4, .mp3, or .wav. Use the _cmdOpenFile(file, path) instead. Syntax of _cmdOpenFile(file, path): where 'file' is the name of the file you want to open (with the extension). You don't need to specify 'path', its default value is '.'. The cmd() and SPL() libraries are linked together, so you can use all the functions from both cmd() and SPL().'''
         self.__author__ = 'Mysterious Ranger'
-        self.__version__ = '1.0.4'
+        self.__version__ = '1.1.3'
         self.SplError = SplError
         self.err_file_path = 'File path not found.'
         self.err_app_path = 'App path not found.'
         self.linux_err = 'Linux version. Command line is disabled.'
     def _ls(self):
 #        self.cmd('ls')
-        raise self.SplError(self.linux_err)
-    def _clear(self):
-#        self.cmd('clear')
-        raise self.SplError(self.linux_err)
+#        raise self.SplError(self.linux_err)
+        os.listdir('.')
+#    def _clear(self):
+##        self.cmd('clear')
+#        raise self.SplError(self.linux_err)
     def _cd(self, folder):
 #        import os
         os.chdir(folder)
 	# Why this is like this, is because self.cmd('cd folder') doesn't work
     def cmd(self, comm):
-        sp.call(comm, shell=True)
+#        sp.call(comm)
+        os.system(comm)
     def _cmdOpenFile(self, file, path='.'):
-#        self._cd(path)
-#        if not self.checkpath(file):
-#            raise self.SplError(self.err_file_path)
-#        self.cmd('open "{0}"'.format(file))
-        raise self.SplError(self.linux_err)
+        self._cd(path)
+        if not self.checkpath(file):
+            raise self.SplError(self.err_file_path)
+#        self.cmd('xdg-open "{0}"'.format(file))
+        self.cmd('see {0}'.format(file)) # sp's call function not reliable
+#        raise self.SplError(self.linux_err)
     def _pwm(self):
         return os.getcwd()
     def checkpath(self, path):
@@ -70,10 +74,7 @@ class SPL(cmd):
 #        if not os.path.exists(file):
 #            raise SplError
 #        sp.Popen([app, file], shell=True)
-    def Open(self, file, app='see'):
-        if not self.checkpath(file):
-            raise self.SplError(self.err_file_path)
-        sp.Popen([app, file], shell=True)
+# NOTE: OPEN() IS REMOVED IN LINUX VERSION. USE _CMDOPENFILE(file, path='.') INSTEAD
     def openApp(self, path):
         if not self.checkpath(path):
             raise self.SplError(self.err_app_path)
